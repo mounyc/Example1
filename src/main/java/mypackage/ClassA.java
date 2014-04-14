8f1b1821e27b2765fdf13ab5933e1d8e0646821f
mounyc <mousumi2850@gmail.com>
2014-04-14 15:45:08 -0400

    Initial commit

---
+++ src/main/java/mypackage/ClassA.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+/**
+ * Created by kona on 4/14/2014.
+ */
+public class ClassA {
+    public static void main(String[] args){
+        System.out.println("MOU.. SUMI");
+    }
+
+}
